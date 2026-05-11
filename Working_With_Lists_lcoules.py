08d72e2a4ab1a17d2c1ebc3eb6e621427399129d
LilLDC <loganco0520@gmail.com>
2026-05-10 23:32:59 -0400

    Upcmong Courses and re-sort

--- Working_With_Lists_lcoules.py
+++ Working_With_Lists_lcoules.py
@@ -1,6 +1,25 @@
-courses = ["Introduction To Security", "Networks & Operating Systems", "Bus Communication Strategies", "Fund Of Cyber Security", "Server Virt & Perf Engine", "Ethical Hacking Strat & Tools"]
+courses = ["Introduction To Security", 
+           "Networks & Operating Systems", 
+           "Bus Communication Strategies", 
+           "Fund Of Cyber Security", 
+           "Server Virt & Perf Engine", 
+           "Ethical Hacking Strat & Tools"
+        ]
 
 courses.sort()
 
 for course in courses:
     print(f"I have taken {course.upper()} at Walsh College.")
+
+courses.append("Advanced Programming")
+courses.append("DB Design & Development")
+courses.append("Digital & Network Forensics")
+
+print("This is my course of with upcoming courses added:")
+
+courses.sort()
+
+print("\nThis is my course of study with upcoming courses added:")
+
+for course in courses:
+    print(course)
