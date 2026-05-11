dec3ad8928625c57f752891b3eb5ba8043a81c7e
LilLDC <loganco0520@gmail.com>
2026-05-10 23:14:53 -0400

    Variables And Simple Data Types Assignment

---
+++ Variables+Simple_Data_Types_lcoules.py
@@ -0,0 +1,35 @@
+# Stores variables using string functions
+first_name = "logan"
+last_name = "COULES"
+num1 = 8.5
+num2 = 3.5
+
+# Prints Variables
+print(f"Hello, {first_name.upper()} {last_name.lower()}")
+
+# Prints 2 new lines
+print("\n")
+
+# Prints output with quotation marks inside the string
+print("\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible - Francis of Assisi\"")
+
+# Stores calculated variables
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Prints num1 + num2 variable
+print(f"{num1} plus {num2} equals {addition}")
+print(f"{num1} minus {num2} equals {subtraction}")
+print(f"{num1} multiplied by {num2} equals {multiplication}")
+print(f"{num1} divided by {num2} equals {division}")
+# Stores month
+
+month = "May"
+day = 10
+
+# Prints using two tabs over on a new line
+print(f"\t\tToday is day {day} of the month of {month}.")
+
+
