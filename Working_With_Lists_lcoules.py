578cadcd43e2d6a51214846f251b06b6100ae8f8
LilLDC <loganco0520@gmail.com>
2026-05-10 23:23:51 -0400

    Start

---
+++ Working_With_Lists_lcoules.py
@@ -0,0 +1,6 @@
+courses = ["Introduction To Security", "Networks & Operating Systems", "Bus Communication Strategies", "Fund Of Cyber Security", "Server Virt & Perf Engine", "Ethical Hacking Strat & Tools"]
+
+courses.sort()
+
+for course in courses:
+    print(course)
