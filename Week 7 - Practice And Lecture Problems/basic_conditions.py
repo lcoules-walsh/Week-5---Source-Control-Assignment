47ec0498a749ddf002302903332383ce790b71b8
LilLDC <loganco0520@gmail.com>
2026-05-17 22:57:45 -0400

    5-2

---
+++ Week 7 - Practice And Lecture Problems/basic_conditions.py
@@ -0,0 +1,16 @@
+# Basic Condition Statements demo - Lcoules
+
+variable1 = 24
+variable2 = "string value of some sort"
+variable3 = 3.14159
+
+if type(variable1) == int:
+    print("The variable is an integer.")
+
+if type(variable2) == str:
+    print("The variable is a string.")
+
+if type(variable3) == float:
+    print("The variable is floating point type.")
+else:
+    print("The variable is something else.")
---
+++ Week 7 - Practice And Lecture Problems/comparing_lists.py
@@ -0,0 +1,7 @@
+# Comparing Lists Demo - lcoules
+
+list1 = ["one", "two", "three"]
+list2 = ["one", "apple", "three", "four"]
+
+if len(list1) < len(list2):
+    print("List 2 is bigger in terms of number of values than list 1")
