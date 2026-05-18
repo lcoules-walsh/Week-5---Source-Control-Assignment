5bfd20f75f1b309fdef3190e3fa6d5ea8391f0ad
LilLDC <loganco0520@gmail.com>
2026-05-17 22:58:59 -0400

    5-3

---
+++ Week 7 - Practice And Lecture Problems/chapter5_exercise.py
@@ -0,0 +1,62 @@
+# Logan Coules
+
+# 5-1
+
+# I predict the expression below will evaluate to True
+color = "blue"
+print(color == "blue")
+
+# I predict the expression below will evaluate to False
+number = 10
+print(number < 5)
+
+# I predict the expression below will evaluate to True
+name = "Logan"
+print(name != "LeBron")
+
+# I predict the expression below will evaluate to False
+temperature = 75
+print(temperature == 100)
+
+# I predict the expression below will evaluate to True
+is_student = True
+print(is_student == True)
+
+# I predict the expression below will evaluate to False
+fruit = "apple"
+print(fruit == "banana")
+
+# 5-2
+
+# This evaluates to True
+number1 = 7
+
+if number1 % 2 == 1:
+    print(str(number1) + " is odd")
+
+# This evaluates to False
+number2 = 8
+
+if number2 % 2 == 1:
+    print(str(number2) + " is odd")
+
+# 5-3
+
+# Odd number example
+number3 = 15
+
+if number3 % 2 == 1:
+    print(str(number3) + " is odd")
+else:
+    number3 += 1
+    print(number3)
+
+# Even number example
+
+number4 = 20
+if number4 % 2 == 1:
+    print(str(number4) + " is odd")
+else:
+    number4 += 1
+    print(number4)
+
